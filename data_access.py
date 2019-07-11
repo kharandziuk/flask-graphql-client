809e69da379abb4dba01716ba32b41b0de897546
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-11 21:30:11 +0200

    refactored and renamed a file with query

---
+++ data_access.py
@@ -0,0 +1,38 @@
+import ssl
+import json
+from graphqlclient import GraphQLClient
+
+# HACK: dirty to overcome certificate problems. DON'T USE IN PRODUCITON
+ssl._create_default_https_context = ssl._create_unverified_context
+
+URL = 'https://web-backend-dev.zeitgold.com/graphql'
+
+client = GraphQLClient(URL)
+def get_data():
+    result = client.execute('''{
+        business(id: "QnVzaW5lc3NOb2RlOjE0Mjc2Y2FhLTA4NmEtNGVmNi04NzMxLTNmYWUzMjE3ZjVlZQ==") {
+            businessTransactions {
+              edges {
+                node {
+                  payables {
+                    edges {
+                      node {
+                        id
+                        amount
+                        referenceId
+                        dateOccurred
+                      }
+                    }
+                  }
+                }
+              }
+            }
+        }
+    }''')
+    result = json.loads(result)
+    flat_nodes = [
+        payables_edges['node']
+        for payable in result['data']['business']['businessTransactions']['edges']
+        for payables_edges in payable['node']['payables']['edges']
+    ]
+    return flat_nodes
