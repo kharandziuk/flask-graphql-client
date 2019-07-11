efa7d0568091643cef41010ee9997f3a62b9cb8f
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-11 19:12:45 +0200

    add validation with marshmallow

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-from marshmallow import Schema, fields, pprint
+from flask import Flask, jsonify, request
+from marshmallow import Schema, fields, ValidationError, utils
 
 app = Flask(__name__)
+
+class LinkSchema(Schema):
+    amount = fields.Number(required=True)
+    payment_reference = fields.Str(required=True)
+    # FIXME: use DateField
+    payment_date = fields.Str(required=True)
+
+link_schema = LinkSchema(unknown=utils.RAISE)
 
 
 @app.route('/link', methods=['POST'])
 def link():
-    return '1'
+    json_data = request.get_json()
+    print(json_data)
+    try:
+        data = link_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 422
+    return jsonify(data)
--- test.py
+++ test.py
@@ -8,5 +8,18 @@ class ExampleTest(TestCase):
         self.w = TestApp(self.app)
 
     def test(self):
-        r = self.w.post('/link')
+        r = self.w.post_json('/link', {
+            'amount': 313,
+            'payment_reference': 'AB1273',
+            'payment_date': '2018-01-13'
+        })
         self.assertEqual(r.text, '1')
+
+    def test_invalid_output(self):
+        r = self.w.post_json('/link', {
+            'amount': 313,
+            'payment_date': '2018-01-13'
+            },
+            expect_errors=True
+        )
+        assert r.status_code == 422
